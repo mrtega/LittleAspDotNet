e2929b80136223e6d0227045c83ca18cd99fa78c
mrtega <tegaodiete@gmail.com>
2019-09-11 21:16:41 +0100

    Add project files.

---
+++ LittleAspDotNet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LittleAspDotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
