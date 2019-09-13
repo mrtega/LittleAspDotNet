3eefa277e4a870110a8b4eda938386edb2dde59e
mrtega <tegaodiete@gmail.com>
2019-09-13 20:15:33 +0100

    Added a service class,

---
+++ LittleAspDotNet/Services/FakeTodoItemService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LittleAspDotNet.Models;
+
+namespace LittleAspDotNet.Services 
+
+{
+    public class FakeTodoItemService : ITodoItemService
+    {
+        public Task<TodoItems[]> GetIncompleteItemsAsync()
+        {
+            var item1 = new TodoItems
+            {
+                Title = "Learn ASP.NET Core",
+                DueAt = DateTimeOffset.Now.AddDays(1)
+            };
+            var item2 = new TodoItems
+            {
+                Title = "Build awesome apps",
+                DueAt = DateTimeOffset.Now.AddDays(2)
+            };
+            return Task.FromResult(new[] { item1, item2 });
+        }
+    }
+}
---
+++ LittleAspDotNet/Services/ITodoItemService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LittleAspDotNet.Models;
+
+namespace LittleAspDotNet.Services
+{
+    interface ITodoItemService
+    {
+        Task<TodoItems[]> GetIncompleteItemsAsync();
+    }
+}
